ca700e4de7ade8b658b1a0c0f44d30ad933c34f7
joshua781467 <67278486+joshua781467@users.noreply.github.com>
2021-10-19 14:56:02 +0530

    Add files via upload

---
+++ gitperm.sh
@@ -0,0 +1,8 @@
+PersonalAccessToken="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"
+
+curl \
+  -X PUT \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: token $PersonalAccessToken"
+  https://api.github.com/orgs/Test-Github007123/teams/devops/repos/joshua781467/ABC123 \
+  -d '{"permission":"admin"}'
