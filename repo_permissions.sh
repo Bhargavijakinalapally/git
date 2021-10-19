54f5bc56393e50a8963d1d4f99f9998af6639334
joshua781467 <67278486+joshua781467@users.noreply.github.com>
2021-10-19 16:31:31 +0530

    Add files via upload

---
+++ repo_permissions.sh
@@ -0,0 +1,15 @@
+UserName="joshua781467"
+PersonalAccessToken="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"
+team_name="Devops"
+org_name="Test-Github007123"
+repo="ABC123"
+
+
+team_id="$(
+    curl -s                                                               \
+         -H "Authorization: token $PersonalAccessToken"                    \
+         "https://api.github.com/orgs/$org_name/teams"                   |\
+      jq 'map(select(.name=="'$team_name'")) | .[].id'
+)"
+
+curl -H "Accept: application/vnd.github.v3+json" -u $UserName:$PersonalAccessToken -X PUT -d '{"permission":"admin"}' https://api.github.com/teams/$team_id/repos/$org_name/$repo
