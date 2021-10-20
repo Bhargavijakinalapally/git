b0cf3e6adaa73f954aab854671f9ad1363a27f3d
joshua781467 <67278486+joshua781467@users.noreply.github.com>
2021-10-20 13:07:59 +0530

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,5 +1,5 @@
  curl \
-  -H "Authorization: token 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" \
+  -H "Authorization: token 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" \
   -X PUT \
   -H "Accept: application/vnd.github.v3+json" \
    https://api.github.com/orgs/Test-Github007123/teams/devops/repos/Test-Github007123/ABC123 \
