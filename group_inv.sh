0bd47f012ebf161b6bfbd1dd138c8365e46f4dd9
joshua781467 <67278486+joshua781467@users.noreply.github.com>
2021-10-20 16:11:10 +0530

    Add files via upload

---
+++ group_inv.sh
@@ -0,0 +1,28 @@
+declare -a email=("joelnathanial777@gmail.com" "joshuabarnesedward@gmail.com")
+org_name=Test-Github007123
+team_name=Devops
+GITHUB_OAUTH_TOKEN="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"
+
+for i in "${email[@]}"
+do
+team_id="$(
+    curl -s                                                               \
+         -H "Authorization: token $GITHUB_OAUTH_TOKEN"                    \
+         "https://api.github.com/orgs/$org_name/teams"                   |\
+      jq 'map(select(.name=="'$team_name'")) | .[].id'
+)"
+json='{
+  "role": "direct_member",
+  "team_ids":['$team_id'],
+  "email":"'$i'"
+}'
+
+echo "$i"
+
+curl -s                                                                   \
+     -H "Authorization: token $GITHUB_OAUTH_TOKEN"                        \
+     -H "Accept: application/vnd.github.dazzler-preview+json"             \
+     -d "$json"                                                           \
+     "https://api.github.com/orgs/$org_name/invitations"
+
+done
