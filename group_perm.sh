ca67a9373313e324b1baae425d18d86657bee9b5
joshua781467 <67278486+joshua781467@users.noreply.github.com>
2021-10-20 16:52:58 +0530

    Add files via upload

---
+++ group_perm.sh
@@ -0,0 +1,14 @@
+  pat="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"
+  org_name=Test-Github007123
+  team_name=developers
+  owner=Test-Github007123
+  repo=ABC123
+  perm="push"
+
+  
+  curl \
+  -H "Authorization: token $pat" \
+  -X PUT \
+  -H "Accept: application/vnd.github.v3+json" \
+   https://api.github.com/orgs/$org_name/teams/$team_name/repos/$owner/$repo \
+  -d '{"permission":"'$perm'"}'
