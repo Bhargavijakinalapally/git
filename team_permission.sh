5bac712825b7576c85755e954917325fa341a88b
joshua781467 <67278486+joshua781467@users.noreply.github.com>
2021-10-20 12:40:15 +0530

    Update team_permission.sh

--- team_permission.sh
+++ team_permission.sh
@@ -5,13 +5,10 @@ repo=ABC123
 PAT="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"
 permission="admin"
 
-json='{
-  "permission":"'$permission'"
-}'
 
 curl -s                                                                              \
      -H "Authorization: token $PAT "                                                 \
      -X PUT                                                                          \
      -H "Accept: application/vnd.github.v3+json"                                     \
      "https://api.github.com/orgs/$orgName/teams/$team/repos/$owner/$repo"          |\
-     -d "$json"
+     -d {"permission":"$permission"}
