7100a78c3dd753c75f9d6f869b12124675461f2e
joshua781467 <67278486+joshua781467@users.noreply.github.com>
2021-10-20 16:57:13 +0530

    Delete test1.sh

--- test1.sh
+++
@@ -1,6 +0,0 @@
- curl \
-  -H "Authorization: token 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" \
-  -X PUT \
-  -H "Accept: application/vnd.github.v3+json" \
-   https://api.github.com/orgs/Test-Github007123/teams/devops/repos/Test-Github007123/ABC123 \
-  -d '{"permission":"triage"}'
