90112ce3d0c579c3fb13d8305af61e23da06ff22
joshua781467 <67278486+joshua781467@users.noreply.github.com>
2021-10-20 12:08:21 +0530

    Add files via upload

---
+++ team_permission.sh
@@ -0,0 +1,15 @@
+orgName="Test-Github007123"
+team="devops"
+owner="Test-Github007123"
+repo="ABC123"
+PAT="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"
+permission="admin"
+
+
+
+curl \
+  -H "Authorization: token $PAT " \
+  -X PUT \
+  -H "Accept: application/vnd.github.v3+json" \
+   https://api.github.com/orgs/$orgName/teams/$team/repos/$owner/$repo \
+  -d '{"permission":"$permission"}'
