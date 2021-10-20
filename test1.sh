9cf527d1bbfb0bf66b664ad1aba62701994e469e
joshua781467 <67278486+joshua781467@users.noreply.github.com>
2021-10-20 13:03:05 +0530

    Create test1.sh

---
+++ test1.sh
@@ -0,0 +1,6 @@
+ curl \
+  -H "Authorization: token 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" \
+  -X PUT \
+  -H "Accept: application/vnd.github.v3+json" \
+   https://api.github.com/orgs/Test-Github007123/teams/devops/repos/Test-Github007123/ABC123 \
+  -d '{"permission":"admin"}'
