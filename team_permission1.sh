31dbb71ebfb881f25eb206bbc8ecc66556cb8523
joshua781467 <67278486+joshua781467@users.noreply.github.com>
2021-10-20 16:56:29 +0530

    Delete team_permission1.sh

--- team_permission1.sh
+++
@@ -1,6 +0,0 @@
-  curl \
-  -H "Authorization: token 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 " \
-  -X PUT \
-  -H "Accept: application/vnd.github.v3+json" \
-   https://api.github.com/orgs/Test-Github007123/teams/devops/repos/Test-Github007123/ABC123 \
-  -d '{"permission":"triage"}'
