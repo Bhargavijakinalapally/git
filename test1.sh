983bc784e0488bb14e9bf687174dab7315d66992
joshua781467 <67278486+joshua781467@users.noreply.github.com>
2021-10-20 13:03:21 +0530

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -3,4 +3,4 @@
   -X PUT \
   -H "Accept: application/vnd.github.v3+json" \
    https://api.github.com/orgs/Test-Github007123/teams/devops/repos/Test-Github007123/ABC123 \
-  -d '{"permission":"admin"}'
+  -d '{"permission":"triage"}'
