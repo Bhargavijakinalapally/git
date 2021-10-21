3c6d1934aad378a6ab4bb5519476e610cbae2d21
joshua781467 <67278486+joshua781467@users.noreply.github.com>
2021-10-21 10:14:22 +0530

    Update group_inv.sh

--- group_inv.sh
+++ group_inv.sh
@@ -1,26 +1,29 @@
+
 declare -a email=("joelnathanial777@gmail.com" "joshuabarnesedward@gmail.com")
 org_name=Test-Github007123
 team_name=Devops
 GITHUB_OAUTH_TOKEN="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"
+
 for i in "${email[@]}"
-do                                                                            \
-    team_id="$(
-        curl -s                                                               \
-             -H "Authorization: token $GITHUB_OAUTH_TOKEN"                    \
-             "https://api.github.com/orgs/$org_name/teams"                   |\
-          jq 'map(select(.name=="'$team_name'")) | .[].id'
-    )"
-    json='{
-      "role": "direct_member",
-      "team_ids":['$team_id'],
-      "email":"'$i'"
-    }'
+do
+team_id="$(
+    curl -s                                                               \
+         -H "Authorization: token $GITHUB_OAUTH_TOKEN"                    \
+         "https://api.github.com/orgs/$org_name/teams"                   |\
+      jq 'map(select(.name=="'$team_name'")) | .[].id'
+)"
+json='{
+  "role": "direct_member",
+  "team_ids":['$team_id'],
+  "email":"'$i'"
+}'
 
-    echo "$i"
+echo "$i"
 
-    curl -s                                                                   \
-         -H "Authorization: token $GITHUB_OAUTH_TOKEN"                        \
-         -H "Accept: application/vnd.github.dazzler-preview+json"             \
-         -d "$json"                                                           \
-         "https://api.github.com/orgs/$org_name/invitations"
+curl -s                                                                   \
+     -H "Authorization: token $GITHUB_OAUTH_TOKEN"                        \
+     -H "Accept: application/vnd.github.dazzler-preview+json"             \
+     -d "$json"                                                           \
+     "https://api.github.com/orgs/$org_name/invitations"
+
 done
