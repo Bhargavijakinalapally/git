bd263a652728020acee8cf8056aa7a247f50f71b
joshua781467 <67278486+joshua781467@users.noreply.github.com>
2021-10-21 10:08:21 +0530

    Update group_inv.sh

--- group_inv.sh
+++ group_inv.sh
@@ -6,24 +6,24 @@ GITHUB_OAUTH_TOKEN="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"
 
 for i in "${email[@]}"
 do
-team_id="$(
-    curl -s                                                               \
-         -H "Authorization: token $GITHUB_OAUTH_TOKEN"                    \
-         "https://api.github.com/orgs/$org_name/teams"                   |\
-      jq 'map(select(.name=="'$team_name'")) | .[].id'
-)"
-json='{
-  "role": "direct_member",
-  "team_ids":['$team_id'],
-  "email":"'$i'"
-}'
+    team_id="$(
+        curl -s                                                               \
+             -H "Authorization: token $GITHUB_OAUTH_TOKEN"                    \
+             "https://api.github.com/orgs/$org_name/teams"                   |\
+          jq 'map(select(.name=="'$team_name'")) | .[].id'
+    )"
+    json='{
+      "role": "direct_member",
+      "team_ids":['$team_id'],
+      "email":"'$i'"
+    }'
 
-echo "$i"
+    echo "$i"
 
-curl -s                                                                   \
-     -H "Authorization: token $GITHUB_OAUTH_TOKEN"                        \
-     -H "Accept: application/vnd.github.dazzler-preview+json"             \
-     -d "$json"                                                           \
-     "https://api.github.com/orgs/$org_name/invitations"
+    curl -s                                                                   \
+         -H "Authorization: token $GITHUB_OAUTH_TOKEN"                        \
+         -H "Accept: application/vnd.github.dazzler-preview+json"             \
+         -d "$json"                                                           \
+         "https://api.github.com/orgs/$org_name/invitations"
 
 done
