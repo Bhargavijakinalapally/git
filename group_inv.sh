6e9d364701f5c72ab6a5851e39c0287fde7a543b
joshua781467 <67278486+joshua781467@users.noreply.github.com>
2021-10-21 10:20:36 +0530

    Update group_inv.sh

--- group_inv.sh
+++ group_inv.sh
@@ -4,7 +4,8 @@ org_name=Test-Github007123
 team_name=Devops
 GITHUB_OAUTH_TOKEN="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"
 
-for i in "${email[@]}"; do
+for i in "${email[@]}"
+do
 team_id="$(
     curl -s                                                               \
          -H "Authorization: token $GITHUB_OAUTH_TOKEN"                    \
