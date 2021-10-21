e75bc6a02b3cceb6cf33e5774bb5b2d1c5cfa6c5
joshua781467 <67278486+joshua781467@users.noreply.github.com>
2021-10-21 09:44:00 +0530

    Update group_inv.sh

--- group_inv.sh
+++ group_inv.sh
@@ -2,7 +2,6 @@ declare -a email=("joelnathanial777@gmail.com" "joshuabarnesedward@gmail.com")
 org_name=Test-Github007123
 team_name=Devops
 GITHUB_OAUTH_TOKEN="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"
-
 for i in "${email[@]}"
 do
 team_id="$(
