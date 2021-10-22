69496d321b9722650315b57b2895c38eebce2a71
joshua781467 <67278486+joshua781467@users.noreply.github.com>
2021-10-22 12:03:12 +0530

    Create group_perm_param.ps1

---
+++ group_perm_param.ps1
@@ -0,0 +1,41 @@
+param (
+    [Parameter(Mandatory = $true)]
+    [String]
+    $org_name,
+
+    [Parameter(Mandatory = $true)]
+    [String]
+    $repo,
+
+    [Parameter(Mandatory = $true)]
+    [String]
+    $team,
+  
+    [Parameter(Mandatory = $true)]
+    [String]
+    $patToken,
+  
+    [Parameter(Mandatory = $true)]
+    [String]
+    $uri = "https://api.github.com/orgs/$org_name/teams/$team/repos/$org_name/$repo",
+  
+    [Parameter(Mandatory = $true)]
+    [String]
+    $permission
+  )
+
+
+
+
+
+
+$headers = @{
+    'Accept' = 'application/vnd.github.v3+json'
+    'Authorization' = $patToken
+}
+
+$body = @{
+'permission' = $permission
+} | ConvertTo-Json
+
+Invoke-RestMethod -Uri $uri -Method PUT -Headers $headers -Body $body
