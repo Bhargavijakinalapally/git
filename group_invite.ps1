e21eb4442f71ca3c9136736ffb6ab8f94fa9f878
joshua781467 <67278486+joshua781467@users.noreply.github.com>
2021-10-22 11:13:27 +0530

    Update group_invite.ps1

--- group_invite.ps1
+++ group_invite.ps1
@@ -1,13 +1,13 @@
 $strarry = @("joshuabarnesedward@gmail.com", "joewick07@gmail.com","jkmramachandran@gmail.com")
 $org_name="Test-Github007123"
 $team="developers"
-$pat="token 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"
+$pat="token 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"
 foreach ($email in $strarry) {
 
 $uri="https://api.github.com/orgs/$org_name/teams/$team"
 $headers = @{
 
-    'Authorization' = 'token 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'
+    'Authorization' = 'token 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'
 }
 $team_full=Invoke-RestMethod -Uri $uri -Method Get -Headers $headers 
 
@@ -24,7 +24,7 @@ $body = @{
 $uri2="https://api.github.com/orgs/$org_name/invitations"
 
 $header2=@{
-    'Authorization' = 'token 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'
+    'Authorization' = 'token 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'
     'Accept' = 'application/vnd.github.dazzler-preview+json'
 }
 
