089c2f611afcfdc77fa3e6e90f6af53f91087903
joshua781467 <67278486+joshua781467@users.noreply.github.com>
2021-10-26 14:40:24 +0530

    Update remove_member_from_team.ps1

--- remove_member_from_team.ps1
+++ remove_member_from_team.ps1
@@ -1,6 +1,7 @@
 $pat= "token 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"
 $org_name="Test-Github007123"
 $name=""
+$team="devops"
 
 $uri="https://api.github.com/orgs/$org_name/members"
 
@@ -15,3 +16,13 @@ $members =Invoke-RestMethod -Uri $uri -Method Get -Headers $headers
 $member_names=[array]$members.login 
 Write-Output $member_names
 
+
+$uri2="https://api.github.com/teams/$team/members/$name"
+
+
+$headers = @{
+    'Accept' = 'application/vnd.github.v3+json'
+    'Authorization' = $pat
+}
+
+Invoke-RestMethod -Uri $uri2 -Method Delete -Headers $headers 
